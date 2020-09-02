b58656bfea6a0718d2688df38c58ac53ecbea513
MinimaxParetoFairness <martin.a.bertran+MMPF@gmail.com>
2020-09-02 17:28:22 -0400

    packaged code branch

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MinimaxParetoFair',
+    version='0.1',
+    packages=['MinimaxParetoFair', 'MinimaxParetoFair.network'],
+    url='',
+    license='GNU',
+    author='Natalia Martinez; Martin Bertran',
+    author_email='nataliamartinez@duke.edu',
+    description=''
+)
